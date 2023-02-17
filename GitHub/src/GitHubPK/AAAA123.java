d893f6e4514883eb0ffb5f8a7c45413a1b5e6edc
Ammy2611 <125531984+Ammy2611@users.noreply.github.com>
2023-02-17 14:20:11 +0530

    Update AAAA123.java

--- GitHub/src/GitHubPK/AAAA123.java
+++ GitHub/src/GitHubPK/AAAA123.java
@@ -5,7 +5,10 @@ public class AAAA123 {
 	public static void main(String[] args) {
 		
 		System.out.println("hii");
-		
+		System.out.print("Amey");
+		System.out.print(" Shewale");
+		System.out.println("Helloo!!!");
+		System.out.println("hii");
 		
 		
 	}
