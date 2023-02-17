3e7049794759154e297854e25c30bf1f42b32aae
WINDOWS <WINDOWS@192.168.0.100>
2023-02-17 14:47:22 +0530

    First Commit

--- GitHub/src/GitHubPK/BBBB123.java
+++ GitHub/src/GitHubPK/BBBB123.java
@@ -3,5 +3,10 @@ package GitHubPK;
 public class BBBB123 {
 public static void main(String[] args) {
 	System.out.println("ABCD");
+	System.out.println("ABCD");
+	System.out.println("ABCD");
+
+	
+//	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
 }
 }
